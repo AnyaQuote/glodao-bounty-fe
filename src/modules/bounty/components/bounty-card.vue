32c532ed5bf532ba7cc979ac30582d0616a07bea
DESKTOP-NK5EIDE\Dao Quoc Toan <daoqtoan@gmail.com>
2022-03-08 09:55:21 +0700

    feat: create frontend layout for bounty list page

---
+++ src/modules/bounty/components/bounty-card.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-hover v-slot="{ hover }">
+    <v-card class="mx-auto rounded-0" outlined>
+      <div
+        :class="{
+          'backdrop-hover-overlay': hover,
+        }"
+      >
+        <div class="pt-4 px-4 position-relative">
+          <v-img height="236" src="https://picsum.photos/200"></v-img>
+          <div class="start-date-container"></div>
+          <div
+            class="start-date-label d-flex justify-end py-2 pr-8 font-weight-600"
+            style="opacity: 1; background: transparent"
+          >
+            Start in Jan 21st 2022 10:00 pm 🔥
+          </div>
+          <div class="position-absolute custom-chevron-flag-container rounded-0">
+            <div class="custom-chevron-flag d-flex flex-column justify-center align-center pt-6 pb-3 elavation-10">
+              <v-icon color="white" size="14" class="mb-2"> mdi-twitter </v-icon>
+            </div>
+          </div>
+        </div>
+        <div class="pa-4">
+          <div>
+            <div class="rounded-circle d-flex justify-center card-project-medium-icon background-blue-diversity">
+              <v-icon color="white">mdi-minus</v-icon>
+            </div>
+            <div class="mt-2 font-family-proxima font-weight-bold card-big-title-text">The Peaky Blinder</div>
+            <div class="custom-dash-divider my-3"></div>
+            <div class="d-flex justify-space-between">
+              <div>Total reward</div>
+              <div class="font-weight-bold">1000 TVN</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="justify-center align-center"
+        :class="{
+          'd-flex  card-hover-overlay': hover,
+          'd-none': !hover,
+        }"
+      >
+        <v-btn width="152" height="124">
+          <div>
+            <div>
+              <v-icon>mdi-crown</v-icon>
+            </div>
+            <br />
+            <div class="text-capitalize font-weight-bold card-big-title-text">Start hunting</div>
+          </div>
+        </v-btn>
+      </div>
+    </v-card>
+  </v-hover>
+</template>
+
+<script lang="ts">
+import { Observer } from 'mobx-vue'
+import { Component, Vue, Ref, Provide, Prop } from 'vue-property-decorator'
+
+@Observer
+@Component
+export default class CurrentTask extends Vue {}
+</script>
+
+<style scoped lang="scss">
+@import url('~@/modules/bounty/styles/index.scss');
+.backdrop-hover-overlay {
+  filter: blur(8px);
+  z-index: -1;
+}
+.card-hover-overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.6);
+  top: 0;
+  left: 0;
+  z-index: 2;
+  border: 1px solid var(--v-bluePrimary-base);
+  filter: blur(0) !important;
+}
+.custom-chevron-flag-container {
+  top: 0;
+  right: 28px;
+  filter: drop-shadow(-1px 6px 3px rgba(50, 50, 0, 0.5));
+}
+.custom-chevron-flag {
+  clip-path: polygon(100% 0, 100% 100%, 50% 82%, 0 100%, 0% 0%);
+  background: var(--v-bluePrimary-base);
+  width: 24px;
+  min-height: 50px;
+}
+.start-date-container {
+  height: 42px;
+  width: 100%;
+  position: absolute;
+  background-color: white;
+  bottom: 0;
+  left: 0;
+  opacity: 0.7;
+}
+.start-date-label {
+  height: 42px;
+  width: 100%;
+  position: absolute;
+  background-color: transparent;
+  bottom: 0;
+  left: 0;
+  font-size: 14px;
+  line-height: 24px;
+  opacity: 1;
+}
+.card-project-medium-icon {
+  width: 32px;
+  height: 32px;
+}
+</style>
--- src/modules/bounty/pages/bounty.vue
+++ src/modules/bounty/pages/bounty.vue
@@ -1,17 +1,106 @@
 <template>
-  <div>Bounty</div>
+  <v-container class="pa-0 ma-0 fill-width" style="max-width: 100% !important">
+    <v-row dense no-gutters>
+      <v-col>
+        <v-sheet height="336" class="page-title-container text-uppercase">
+          <div>Bounty</div>
+          <div class="mt-4 font-italic">Hunter</div>
+        </v-sheet>
+        <v-divider />
+      </v-col>
+    </v-row>
+    <v-row dense no-gutters>
+      <v-col>
+        <v-container>
+          <v-row>
+            <v-col class="mt-10 section-big-title-text font-weight-bold" cols="12">Your current task</v-col>
+            <v-col lg="3" md="6" sm="12" cols="12">
+              <current-task />
+            </v-col>
+            <v-col lg="3" md="6" sm="12" cols="12">
+              <current-task />
+            </v-col>
+            <v-col lg="3" md="6" sm="12" cols="12">
+              <current-task />
+            </v-col>
+            <v-col lg="3" md="6" sm="12" cols="12">
+              <current-task />
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-divider class="mt-8" />
+      </v-col>
+    </v-row>
+    <v-row dense>
+      <v-col>
+        <v-container class="mt-10">
+          <v-row>
+            <v-col>
+              <div class="section-big-title-text font-weight-bold">Bounty hunters (32)</div>
+            </v-col>
+            <v-col cols="12" lg="2" md="3" sm="3" xs="4">
+              <v-select :items="items" label="Start time" outlined dense class="rounded-0"></v-select>
+            </v-col>
+            <v-col cols="12" lg="2" md="3" sm="3" xs="4">
+              <v-select :items="items" label="Recently added" outlined dense class="rounded-0"></v-select>
+            </v-col>
+          </v-row>
+          <v-row class="mt-8">
+            <v-col md="4" sm="6" cols="12">
+              <bounty-card />
+            </v-col>
+            <v-col md="4" sm="6" cols="12">
+              <bounty-card />
+            </v-col>
+            <v-col md="4" sm="6" cols="12">
+              <bounty-card />
+            </v-col>
+          </v-row>
+          <v-row dense no-gutters>
+            <v-col>
+              <div class="my-10 d-flex justify-center align-center">
+                <v-btn outlined class="rounded-0 font-weight-600 button-small text-capitalize">
+                  Loadmore (118 bounty hunters)
+                </v-btn>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Observer } from 'mobx-vue'
-import { Component, Vue, Provide, Watch } from 'vue-property-decorator'
+import { Component, Vue, Ref, Provide } from 'vue-property-decorator'
 import { walletStore } from '@/stores/wallet-store'
+import { BountyHunterViewModel } from '@/modules/bounty/viewmodels/bounty-hunter-viewmodel'
 
 @Observer
 @Component({
-  components: {},
+  components: {
+    'current-task': () => import('@/modules/bounty/components/current-task.vue'),
+    'bounty-card': () => import('@/modules/bounty/components/bounty-card.vue'),
+  },
 })
-export default class Staking extends Vue {}
+export default class Farming extends Vue {
+  @Provide() vm = new BountyHunterViewModel()
+  walletStore = walletStore
+  items = ['Foo', 'Bar', 'Fizz', 'Buzz']
+}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import url('~@/modules/bounty/styles/index.scss');
+.button-small {
+  font-size: 14px;
+  line-height: 24px;
+}
+.page-title-container {
+  padding: 86px 0 106px 178px;
+  font-size: 72px;
+  line-height: 64px;
+  font-weight: 700;
+}
+</style>
