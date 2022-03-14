a8856754cf3eceff88eab7f9d3d1279b74074e26
Dao Quoc Toan <daoqtoan@gmail.com>
2022-03-14 12:04:26 +0700

    feat: store jwt and user in local storage

--- src/helpers/local-data.ts
+++ src/helpers/local-data.ts
@@ -1,7 +1,22 @@
-
-
 class LocalData {
- 
+  get user(): any {
+    return JSON.parse(localStorage.getItem('gloDaoUser') || '{}')
+  }
+  set user(value: any) {
+    localStorage.setItem('gloDaoUser', JSON.stringify(value))
+  }
+
+  get jwt(): string {
+    if (localStorage.getItem('gloDaoJwt')) return JSON.parse(localStorage.getItem('gloDaoJwt') || '')
+    return ''
+  }
+  set jwt(value: string) {
+    localStorage.setItem('gloDaoJwt', JSON.stringify(value))
+  }
+
+  reset() {
+    localStorage.clear()
+  }
 }
 
 export const localdata = new LocalData()
