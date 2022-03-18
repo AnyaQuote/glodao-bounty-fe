114d6f0361fa11f859e03689fdfa2b4c00b5d454
Dao Quoc Toan <daoqtoan@gmail.com>
2022-03-18 09:16:48 +0700

    feat: add copyright line to footer

--- src/components/company-footer.vue
+++ src/components/company-footer.vue
@@ -1,46 +1,49 @@
 <template>
-  <div
-    class="footer-background"
-    :class="{
-      footer: $vuetify.breakpoint.smAndUp,
-    }"
-  >
-    <v-container class="px-6 py-7">
-      <v-row>
-        <v-col cols="12" sm="6" class="d-flex flex-column">
-          <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            :src="require(`@/assets/images/glodao-logo.svg`)"
-            transition="scale-transition"
-            width="75"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          sm="6"
-          class="d-flex align-center"
-          :class="{
-            'justify-end': $vuetify.breakpoint.smAndUp,
-            'justify-start': $vuetify.breakpoint.xs,
-          }"
-        >
-          <a href="">
-            <v-icon> fab fa-medium-m </v-icon>
-          </a>
-          <a href="" class="ml-8">
-            <v-icon> fab fa-telegram </v-icon>
-          </a>
-          <a href="" class="ml-8">
-            <v-icon> fab fa-twitter </v-icon>
-          </a>
-          <a href="" class="ml-8">
-            <v-icon> fab fa-telegram-plane </v-icon>
-          </a>
-        </v-col>
-      </v-row>
-    </v-container>
+  <div>
+    <div
+      class="footer-background"
+      :class="{
+        footer: $vuetify.breakpoint.smAndUp,
+      }"
+    >
+      <v-container class="px-6 py-7">
+        <v-row>
+          <v-col cols="12" sm="6" class="d-flex flex-column">
+            <v-img
+              alt="Vuetify Logo"
+              class="shrink mr-2"
+              contain
+              :src="require(`@/assets/images/glodao-logo.svg`)"
+              transition="scale-transition"
+              width="75"
+            />
+          </v-col>
+          <v-col
+            cols="12"
+            sm="6"
+            class="d-flex align-center"
+            :class="{
+              'justify-end': $vuetify.breakpoint.smAndUp,
+              'justify-start': $vuetify.breakpoint.xs,
+            }"
+          >
+            <a href="">
+              <v-icon> fab fa-medium-m </v-icon>
+            </a>
+            <a href="" class="ml-8">
+              <v-icon> fab fa-telegram </v-icon>
+            </a>
+            <a href="" class="ml-8">
+              <v-icon> fab fa-twitter </v-icon>
+            </a>
+            <a href="" class="ml-8">
+              <v-icon> fab fa-telegram-plane </v-icon>
+            </a>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+    <div class="flex-center-box neutral15 py-3">Copyright © 2022 GLO DAO. All rights reserved.</div>
   </div>
 </template>
 
