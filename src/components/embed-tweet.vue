667b4cacf6d8c544e84c5bf6ea5da6de38bed28f
Dao Quoc Toan <daoqtoan@gmail.com>
2022-04-12 15:15:44 +0700

    feat: create embed tweet link component

---
+++ src/components/embed-tweet.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="fill-width">
+    <div
+      class="fill-width justify-center"
+      v-show="$vuetify.theme.dark"
+      :class="{
+        'd-flex': $vuetify.theme.dark,
+      }"
+    >
+      <blockquote class="twitter-tweet" data-theme="dark">
+        <a :href="`${link}?ref_src=twsrc%5Etfw`" target="_blank">
+          Loading tweet data <v-progress-circular indeterminate color="primary" size="24"></v-progress-circular>
+        </a>
+      </blockquote>
+    </div>
+    <div
+      class="fill-width justify-center"
+      v-show="!$vuetify.theme.dark"
+      :class="{
+        'd-flex': !$vuetify.theme.dark,
+      }"
+    >
+      <blockquote class="twitter-tweet" data-theme="light">
+        <a :href="`${link}?ref_src=twsrc%5Etfw`" target="_blank">
+          Loading tweet data <v-progress-circular indeterminate color="primary" size="24"></v-progress-circular>
+        </a>
+      </blockquote>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Observer } from 'mobx-vue'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Observer
+@Component({
+  components: {},
+})
+export default class EmbedTweet extends Vue {
+  @Prop({ required: true }) link!: any
+
+  beforeCreate() {
+    if (!document.getElementById('embedTweetScript')) {
+      const embedTweetScript = document.createElement('script')
+      embedTweetScript.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+      embedTweetScript.setAttribute('id', 'embedTweetScript')
+      document.head.appendChild(embedTweetScript)
+    }
+  }
+}
+</script>
+<style scoped></style>
