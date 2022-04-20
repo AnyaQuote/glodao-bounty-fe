270a0bcf983bde4aaed84430955e349974d7c9c0
duchm22 <duchm22.se@gmail.com>
2022-04-20 16:59:35 +0700

    style: remove hover

--- src/modules/bounty/components/bounty-upcomming.vue
+++ src/modules/bounty/components/bounty-upcomming.vue
@@ -5,73 +5,73 @@
     </v-col>
     <template v-if="$vuetify.breakpoint.mdAndUp">
       <v-col cols="12">
-        <v-hover v-slot="{ hover }">
-          <div class="p-relative">
-            <v-tabs-items v-model="tab" class="rounded-lg">
-              <!-- Background image start (This width and height will decide the structure) -->
-              <v-sheet class="fill-height p-relative">
-                <v-tab-item v-for="(pool, index) in upcomingPoolList" :value="index" :key="pool.id">
-                  <v-img height="700" :src="pool.metadata.coverImage" aspect-ratio="1.5"></v-img>
+        <!-- <v-hover v-slot="{ hover }"> -->
+        <div class="p-relative">
+          <v-tabs-items v-model="tab" class="rounded-lg">
+            <!-- Background image start (This width and height will decide the structure) -->
+            <v-sheet class="fill-height p-relative">
+              <v-tab-item v-for="(pool, index) in upcomingPoolList" :value="index" :key="pool.id">
+                <v-img height="700" :src="pool.metadata.coverImage" aspect-ratio="1.5"></v-img>
+              </v-tab-item>
+            </v-sheet>
+            <!-- Background image end -->
+          </v-tabs-items>
+
+          <!-- info header start -->
+          <div class="d-flex p-absolute t-b-l-r-0 ma-10 mr-0">
+            <div class="d-flex flex-column justify-end" style="flex-grow: 1">
+              <v-tabs-items v-model="tab">
+                <v-tab-item v-for="(pool, index) in upcomingPoolList" :value="index" :key="pool.id" eager>
+                  <bounty-upcoming-card
+                    :name="pool.name"
+                    :startTime="pool.startTime"
+                    :endTime="pool.endTime"
+                    :rewardAmount="pool.rewardAmount"
+                    :chainId="pool.chainId"
+                    :maxParticipant="pool.maxParticipant"
+                    :metadata="pool.metadata"
+                    :retract="showSide"
+                  ></bounty-upcoming-card>
                 </v-tab-item>
-              </v-sheet>
-              <!-- Background image end -->
-            </v-tabs-items>
+              </v-tabs-items>
+            </div>
+            <!-- info header end -->
 
-            <!-- info header start -->
-            <div class="d-flex p-absolute t-b-l-r-0 ma-10 mr-0">
-              <div class="d-flex flex-column justify-end" style="flex-grow: 1">
-                <v-tabs-items v-model="tab">
-                  <v-tab-item v-for="(pool, index) in upcomingPoolList" :value="index" :key="pool.id" eager>
-                    <bounty-upcoming-card
-                      :name="pool.name"
-                      :startTime="pool.startTime"
-                      :endTime="pool.endTime"
-                      :rewardAmount="pool.rewardAmount"
-                      :chainId="pool.chainId"
-                      :maxParticipant="pool.maxParticipant"
-                      :metadata="pool.metadata"
-                      :hover="hover"
-                    ></bounty-upcoming-card>
-                  </v-tab-item>
-                </v-tabs-items>
+            <!-- slider button start -->
+            <div v-show="!showSide" class="p-absolute t-b-l-r-0 flex-center-end">
+              <div class="test rounded-tl-lg rounded-bl-lg py-8" @click="toggleSide">
+                <v-icon size="50">mdi-chevron-left</v-icon>
               </div>
-              <!-- info header end -->
+            </div>
+            <!-- slider button end -->
 
-              <!-- slider button start -->
-              <div v-show="!hover" class="p-absolute t-b-l-r-0 flex-center-end">
-                <div class="test rounded-tl-lg rounded-bl-lg py-8">
-                  <v-icon size="50">mdi-chevron-left</v-icon>
-                </div>
-              </div>
-              <!-- slider button end -->
-
-              <!-- slider start -->
-              <v-slide-x-reverse-transition>
-                <div v-show="hover" class="p-absolute t-r-0 fill-height">
-                  <!-- <div class="d-flex align-center justify-end">
-                    <div class="test rounded-tl-lg rounded-bl-lg py-8">
-                      <v-icon size="50">mdi-chevron-left</v-icon>
-                    </div>
-                  </div> -->
-                  <div
-                    class="pa-3 scrollable fill-height transparent test rounded-tl-lg rounded-bl-lg"
-                    style="width: 218px"
-                  >
-                    <BountyUpcomingPreview
-                      v-for="(pool, index) in upcomingPoolList"
-                      @onClick="toggleTab"
-                      :metadata="pool.metadata"
-                      :name="pool.name"
-                      :id="pool.id"
-                      :key="index"
-                    ></BountyUpcomingPreview>
+            <!-- slider start -->
+            <v-slide-x-reverse-transition>
+              <div v-show="showSide" class="flex-center p-absolute t-r-0 fill-height">
+                <div class="d-flex align-center justify-end">
+                  <div class="test rounded-tl-lg rounded-bl-lg py-8" @click="toggleSide">
+                    <v-icon size="50">mdi-chevron-left</v-icon>
                   </div>
                 </div>
-              </v-slide-x-reverse-transition>
-              <!-- slider end -->
-            </div>
+                <div
+                  class="pa-3 scrollable fill-height transparent test rounded-tl-lg rounded-bl-lg"
+                  style="width: 218px"
+                >
+                  <BountyUpcomingPreview
+                    v-for="(pool, index) in upcomingPoolList"
+                    @onClick="toggleTab"
+                    :metadata="pool.metadata"
+                    :name="pool.name"
+                    :id="pool.id"
+                    :key="index"
+                  ></BountyUpcomingPreview>
+                </div>
+              </div>
+            </v-slide-x-reverse-transition>
+            <!-- slider end -->
           </div>
-        </v-hover>
+        </div>
+        <!-- </v-hover> -->
       </v-col>
     </template>
     <template v-else>
@@ -111,7 +111,10 @@ export default class BountyUpcoming extends Vue {
   toggleTab(tabIndex: number) {
     this.tab = tabIndex
   }
-  toggleSide = false
+  showSide = false
+  toggleSide() {
+    this.showSide = !this.showSide
+  }
 }
 </script>
 
