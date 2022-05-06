b67392559aeb495f48fb6e0e4e514562a7195136
duchm22 <duchm22.se@gmail.com>
2022-05-06 16:51:06 +0700

    fix: remove loading component exist effect

--- src/modules/account/components/loading-component.vue
+++ src/modules/account/components/loading-component.vue
@@ -1,9 +1,7 @@
 <template>
-  <v-fade-transition>
-    <v-sheet v-if="loading" height="320" outlined class="d-flex align-center justify-center neutral100--bg" rounded>
-      <v-progress-circular lo :size="70" :width="7" color="bluePrimary" indeterminate></v-progress-circular>
-    </v-sheet>
-  </v-fade-transition>
+  <v-sheet v-if="loading" height="320" outlined class="d-flex align-center justify-center neutral100--bg" rounded>
+    <v-progress-circular lo :size="70" :width="7" color="bluePrimary" indeterminate></v-progress-circular>
+  </v-sheet>
 </template>
 
 <script lang="ts">
