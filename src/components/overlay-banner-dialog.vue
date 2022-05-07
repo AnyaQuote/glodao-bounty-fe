8c44573a91d28a88a52662537761daec6f60ce06
Dao Quoc Toan <daoqtoan@gmail.com>
2022-05-07 16:46:33 +0700

    feat: create popup banner

---
+++ src/components/overlay-banner-dialog.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-dialog :value="true" width="80%">
+    <v-img :src="require('@/assets/images/popup-01.jpg')" @click="openBannerLink"></v-img>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class OverlayBannerDialog extends Vue {
+  openBannerLink = () => {
+    // window.open('https://medium.com', '_blank')
+  }
+}
+</script>
+
+<style scoped></style>
