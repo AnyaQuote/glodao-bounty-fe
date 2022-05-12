f04567add8ed906f0ec4400dc52de4c6915b32c3
Dao Quoc Toan <daoqtoan@gmail.com>
2022-05-12 15:19:58 +0700

    feat: add constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 import { FixedNumber } from '@ethersproject/bignumber'
-export const Zero = FixedNumber.from(0)
+export const Zero = FixedNumber.from('0')
+export const HUNDRED = FixedNumber.from('100')
 
 export const TWEET_MIN_LENGTH = 20
 
