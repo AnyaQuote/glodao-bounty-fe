279fe1bb7962e6a41baaed0610a2ed163727f0ee
duchm22 <duchm.se22@gmail.com>
2022-07-03 20:34:50 +0700

    chore: create app trial app link UI component

---
+++ src/modules/bounty/components/app-trial/at-app-link.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-sheet class="d-flex flex-column flex-sm-row neutral100--bg pa-7 mt-7 mt-sm-10" outlined rounded="lg">
+    <div class="d-flex flex-column flex-grow-1 text-h6 font-weight-regular mb-2 mb-sm-0">
+      <span>Available on App Store and Google Play</span>
+      <span>Download to join mission</span>
+    </div>
+    <v-img max-width="200" height="60" src="@/assets/images/appstore.png" />
+    <div class="py-1 py-sm-0 px-sm-3" />
+    <v-img max-width="200" height="60" src="@/assets/images/googleplay.png" />
+  </v-sheet>
+</template>
+
+<script lang="ts">
+import { Observer } from 'mobx-vue'
+import { Component, Inject, Vue } from 'vue-property-decorator'
+import { BountyAppTrialViewModel } from '../../viewmodels/bounty-app-trial-viewmodel'
+
+@Observer
+@Component
+export default class AppTrialAppLink extends Vue {
+  @Inject() vm!: BountyAppTrialViewModel
+}
+</script>
+
+<style scoped></style>
