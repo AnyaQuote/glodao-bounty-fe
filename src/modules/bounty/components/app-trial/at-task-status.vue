f40ae42da04a200418dd5e8408521163ac8b9d1c
duchm22 <duchm.se22@gmail.com>
2022-07-03 23:38:14 +0700

    chore: create task status UI sub component

---
+++ src/modules/bounty/components/app-trial/at-task-status.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="d-flex align-center">
+      <v-sheet
+        v-if="!task.isCompleted"
+        width="40"
+        height="40"
+        rounded="lg"
+        class="font-weight-bold neutral15 text-h6 d-flex"
+      >
+        <span class="ma-auto">{{ task.step }}</span>
+      </v-sheet>
+      <v-sheet v-else width="40" height="40" rounded="lg" class="d-flex success">
+        <v-icon color="white" class="ma-auto" size="35">mdi-check</v-icon>
+      </v-sheet>
+      <div class="text-h6 font-weight-bold flex-grow-1 ml-4 text-truncate">{{ task.name }}</div>
+      <v-icon color="bluePrimary">mdi-open-in-new</v-icon>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Observer } from 'mobx-vue'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+const defaultProp = () => ({})
+
+interface TaskStatus {
+  step?: number
+  name?: string
+  isCompleted?: boolean
+}
+
+@Observer
+@Component
+export default class AppTrialTaskStatus extends Vue {
+  @Prop({ default: defaultProp }) task!: TaskStatus
+}
+</script>
+
+<style scoped></style>
