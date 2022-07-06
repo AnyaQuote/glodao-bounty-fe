57023c9d0a4c76093f266d28419cdb2cdc439811
duchm22 <duchm.se22@gmail.com>
2022-07-06 11:55:13 +0700

    style: small spacing on small screen

--- src/modules/bounty/components/app-trial/at-bounty-task-stats.vue
+++ src/modules/bounty/components/app-trial/at-bounty-task-stats.vue
@@ -2,7 +2,7 @@
   <div class="mt-10 d-flex flex-column flex-sm-row">
     <!-- ======================= TASK STATS START ======================= -->
     <div
-      class="order-last order-sm-first d-flex flex-column bluePrimary flex-grow-1 pa-7 rounded-xl"
+      class="order-last order-sm-first d-flex flex-column bluePrimary flex-grow-1 pa-sm-7 pa-4 rounded-xl"
       :class="{ 'lighten-3': $vuetify.theme.dark }"
     >
       <div class="d-flex flex-column flex-sm-row align-center align-sm-end white--text">
@@ -16,7 +16,7 @@
     <div class="mx-0 mx-sm-3 my-2 my-sm-0" />
     <!-- ======================= POOL STATS START ======================= -->
     <v-sheet class="order-first order-sm-last neutral100--bg align-self-sm-start" rounded="xl" outlined>
-      <div class="pa-6 rounded-t-xl">
+      <div class="pa-4 pa-sm-6 rounded-t-xl">
         <div class="text-h5 font-weight-bold bluePrimary--text mb-6">Pool update</div>
         <div class="d-flex align-center text-h6 justify-space-between">
           <span class="font-weight-regular">Participants</span>
@@ -37,7 +37,7 @@
           height="30"
         />
       </div>
-      <div class="pa-6 bluePrimary lighten-3 rounded-xl">
+      <div class="pa-4 pa-sm-6 bluePrimary lighten-3 rounded-xl">
         <div class="bluePrimary--text text-uppercase font-weight-bold text-center mb-4">Pool ends in</div>
         <countdown :targetDate="vm.taskEndTime" />
       </div>
