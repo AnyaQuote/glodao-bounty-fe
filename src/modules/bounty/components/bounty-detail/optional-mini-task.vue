560e1d1d9e032760b0eaff7ca3e627a2c50e801c
Bang Bu <daoqtoan@gmail.com>
2022-07-16 10:12:20 +0700

    feat: create temp optional task

---
+++ src/modules/bounty/components/bounty-detail/optional-mini-task.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <optional-task :task="task" :step="step" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Observer } from 'mobx-vue'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { get } from 'lodash-es'
+
+@Observer
+@Component({
+  components: {
+    'optional-task': () => import('@/modules/bounty/components/bounty-detail/optional-task/optional-task.vue'),
+  },
+})
+export default class BountyDetail extends Vue {
+  @Prop({ required: true }) task!: any
+  @Prop({ required: true }) step!: number
+  type = get(this.task, 'type', '')
+  title = ''
+}
+</script>
+<style scoped>
+.greenSenamatic--bg {
+  background: var(--v-greenSenamatic-base);
+}
+.neutral20--bg {
+  background: var(--v-neutral20-base);
+}
+.v-btn--disabled {
+  background-image: none !important;
+}
+</style>
