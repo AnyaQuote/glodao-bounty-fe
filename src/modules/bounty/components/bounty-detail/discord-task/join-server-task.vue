23c7ccddcaafa17bc4d19645d2b385270d809a57
Bang Bu <daoqtoan@gmail.com>
2022-07-17 20:13:24 +0700

    quick fix: update discord task guide

--- src/modules/bounty/components/bounty-detail/discord-task/join-server-task.vue
+++ src/modules/bounty/components/bounty-detail/discord-task/join-server-task.vue
@@ -122,7 +122,7 @@
               >
               your referral link
             </li>
-            <li>Chat something in GloDAO Discord Server</li>
+            <li>Chat something in the {{ page }} Server</li>
           </ol>
         </div>
       </v-sheet>
