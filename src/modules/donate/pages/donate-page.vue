7f41ebb46ba2dc5f979c975d7fcec08da85090dd
Le Sy Thanh Long <longtennis2000@gmail.com>
2022-08-29 15:13:28 +0700

    feat: create base donate screen layout

---
+++ src/modules/donate/pages/donate-page.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Observer } from 'mobx-vue'
+import { Component, Vue, Provide } from 'vue-property-decorator'
+import { walletStore } from '@/stores/wallet-store'
+import { authStore } from '@/stores/auth-store'
+import { DonateViewModel } from '@/modules/donate/viewmodels/donate-viewmodel'
+import moment from 'moment'
+
+@Observer
+@Component({
+  components: {},
+})
+export default class DonatePage extends Vue {
+  @Provide() vm = new DonateViewModel()
+  walletStore = walletStore
+  authStore = authStore
+  moment = moment
+}
+</script>
