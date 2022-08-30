c4ccef67cd750b69038ac02109c2529427ca0149
Bang Bu <daoqtoan@gmail.com>
2022-08-30 21:17:54 +0700

    efat: add cursor pointer to image

--- src/modules/bounty/pages/bounty.vue
+++ src/modules/bounty/pages/bounty.vue
@@ -2,7 +2,9 @@
   <v-row dense no-gutters>
     <v-col cols="12">
       <!-- <bounty-banner></bounty-banner> -->
-      <v-img :src="require('@/assets/images/donationPageBanner.jpg')"></v-img>
+      <router-link to="/donate" class="cursor-pointer">
+        <v-img :src="require('@/assets/images/donationPageBanner.jpg')"></v-img>
+      </router-link>
     </v-col>
     <!-- <v-col cols="12" v-if="authStore.jwt && vm.currentTaskList.length > 0">
       <v-container>
