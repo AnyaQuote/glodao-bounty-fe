7157d3b776c0ba0b4f273a6e41ff5a055b7a15b4
Bang Bu <daoqtoan@gmail.com>
2022-08-30 19:33:26 +0700

    feat: add banner for donation page

--- src/modules/donate/pages/donate-page.vue
+++ src/modules/donate/pages/donate-page.vue
@@ -1,66 +1,70 @@
 <template>
-  <v-container>
-    <div class="d-flex align-center justify-center mt-6">
-      <v-card class="neutral100 border-radius-16 pa-8" elevation="3" style="max-width: 555px" outlined>
-        <v-container>
-          <div class="d-flex align-center justify-center">
-            <img :src="require('@/assets/images/reward.png')" alt="" />
-            <div class="page-title ml-4">Support us</div>
-          </div>
-          <div class="mt-4 py-2 text-center">
-            <v-icon class="mr-2" left size="24">mdi-wallet-outline</v-icon>
-            <span v-if="!vm.account">You need to connect your wallet first</span>
-            <span class="address" v-if="$vuetify.breakpoint.smAndUp">
-              {{ vm.account }}
-            </span>
-            <span class="address" v-else>
-              {{ vm.account | shortAddress(6, 6) }}
-            </span>
-          </div>
-          <div class="py-6 px-2 text-center">
-            <div :class="{ 'd-flex align-center justify-center': $vuetify.breakpoint.mdAndUp }">
-              <v-card
-                @click="vm.changeDonationAmount(amount)"
-                class="border-radius-8 py-6 px-2 text-center ma-2"
-                :class="{
-                  'linear-background-blue-main white--text': amount === vm.amount,
-                  'neutral100--bg primary--text': amount !== vm.amount,
-                }"
-                style="min-width: 80px"
-                v-for="amount in vm.amountList"
-                :key="amount"
-              >
-                {{ amount }}$
-              </v-card>
+  <div>
+    <div class="fill-width">
+      <v-img :src="require('@/assets/images/donationPageBanner.jpg')"></v-img>
+    </div>
+    <v-container>
+      <div class="d-flex align-center justify-center mt-6">
+        <v-card class="neutral100 border-radius-16 pa-8" elevation="3" style="max-width: 555px" outlined>
+          <v-container>
+            <div class="d-flex align-center justify-center">
+              <img :src="require('@/assets/images/reward.png')" alt="" />
+              <div class="page-title ml-4">Support us</div>
             </div>
-          </div>
-          <div class="ml-5">
-            <span class="text-body-2 font-italic text-decoration-underline"
-              >Donate to support us and earn a chance to receive our exclusive NFT</span
-            >
-          </div>
-          <div class="mt-1 py-4 text-center">
-            <div :class="{ 'd-flex align-center justify-space-around': $vuetify.breakpoint.mdAndUp }">
-              <div>
-                <v-btn
-                  depressed
-                  :class="buttonClass"
-                  large
-                  :block="$vuetify.breakpoint.xs"
-                  :disabled="vm.shouldDisableDonate"
-                  @click="vm.donate()"
+            <div class="mt-4 py-2 text-center">
+              <v-icon class="mr-2" left size="24">mdi-wallet-outline</v-icon>
+              <span v-if="!vm.account">You need to connect your wallet first</span>
+              <span class="address" v-if="$vuetify.breakpoint.smAndUp">
+                {{ vm.account }}
+              </span>
+              <span class="address" v-else>
+                {{ vm.account | shortAddress(6, 6) }}
+              </span>
+            </div>
+            <div class="py-6 px-2 text-center">
+              <div :class="{ 'd-flex align-center justify-center': $vuetify.breakpoint.mdAndUp }">
+                <v-card
+                  @click="vm.changeDonationAmount(amount)"
+                  class="border-radius-8 py-6 px-2 text-center ma-2"
+                  :class="{
+                    'linear-background-blue-main white--text': amount === vm.amount,
+                    'neutral100--bg primary--text': amount !== vm.amount,
+                  }"
+                  style="min-width: 80px"
+                  v-for="amount in vm.amountList"
+                  :key="amount"
                 >
-                  Donate
-                </v-btn>
+                  {{ amount }}$
+                </v-card>
               </div>
             </div>
-          </div>
-        </v-container>
-      </v-card>
-    </div>
-    <div :class="{ 'd-flex align-center justify-space-around ': $vuetify.breakpoint.mdAndUp }">
-      <div class="mt-8" style="min-width: 300px">
-        <!-- <div class="page-title">Donation history</div>
+            <div class="ml-5">
+              <span class="text-body-2 font-italic text-decoration-underline"
+                >Donate to support us and earn a chance to receive our exclusive NFT</span
+              >
+            </div>
+            <div class="mt-1 py-4 text-center">
+              <div :class="{ 'd-flex align-center justify-space-around': $vuetify.breakpoint.mdAndUp }">
+                <div>
+                  <v-btn
+                    depressed
+                    :class="buttonClass"
+                    large
+                    :block="$vuetify.breakpoint.xs"
+                    :disabled="vm.shouldDisableDonate"
+                    @click="vm.donate()"
+                  >
+                    Donate
+                  </v-btn>
+                </div>
+              </div>
+            </div>
+          </v-container>
+        </v-card>
+      </div>
+      <div :class="{ 'd-flex align-center justify-space-around ': $vuetify.breakpoint.mdAndUp }">
+        <div class="mt-8" style="min-width: 300px">
+          <!-- <div class="page-title">Donation history</div>
         <v-card class="pa-8" flat elevation="3">
           <div>
             <v-data-table
@@ -72,9 +76,9 @@
             ></v-data-table>
           </div>
         </v-card> -->
-      </div>
-      <div class="mt-8" style="min-width: 300px">
-        <!-- <div class="page-title">Your donation history</div>
+        </div>
+        <div class="mt-8" style="min-width: 300px">
+          <!-- <div class="page-title">Your donation history</div>
         <v-card class="pa-8" flat elevation="3">
           <div>
             <v-data-table
@@ -87,9 +91,10 @@
             ></v-data-table>
           </div>
         </v-card> -->
+        </div>
       </div>
-    </div>
-  </v-container>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
