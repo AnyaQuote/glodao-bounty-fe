fdf5569be708498d2970e614cccf225467e1eabc
Le Sy Thanh Long <longtennis2000@gmail.com>
2022-08-30 15:08:25 +0700

    Edit table and add some text

--- src/modules/donate/pages/donate-page.vue
+++ src/modules/donate/pages/donate-page.vue
@@ -7,8 +7,17 @@
             <img :src="require('@/assets/images/reward.png')" alt="" />
             <div class="page-title ml-4">Support us</div>
           </div>
-          <div class="mt-4 border-radius-8 py-6 px-2 text-center sub-card">
-            <div :class="{ 'd-flex align-center': $vuetify.breakpoint.mdAndUp }">
+          <div class="mt-4 py-2 text-center">
+            <v-icon class="mr-2" left size="24">mdi-wallet-outline</v-icon>
+            <span class="address" v-if="$vuetify.breakpoint.smAndUp">
+              {{ walletStore.account }}
+            </span>
+            <span class="address" v-else>
+              {{ walletStore.account | shortAddress(6, 6) }}
+            </span>
+          </div>
+          <div class="py-6 px-2 text-center">
+            <div :class="{ 'd-flex align-center justify-center': $vuetify.breakpoint.mdAndUp }">
               <v-card
                 @click="changeColor('button')"
                 class="border-radius-8 py-6 px-2 text-center ma-2"
@@ -43,6 +52,10 @@
               </v-card>
             </div>
           </div>
+          <div class="ml-5">
+            <span>lorem ipsum dolor sit am id urna et ullamcorper lorem ipsum 
+              dolor sit am id urna et ull lorem</span>
+          </div>
           <div class="mt-1 py-4 text-center">
             <div :class="{ 'd-flex align-center justify-space-around': $vuetify.breakpoint.mdAndUp }">
               <div>
@@ -55,7 +68,7 @@
         </v-container>
       </v-card>
     </div>
-    <div :class="{'d-flex align-center justify-space-around ' : $vuetify.breakpoint.mdAndUp}">
+    <div :class="{ 'd-flex align-center justify-space-around ': $vuetify.breakpoint.mdAndUp }">
       <div class="mt-8" style="min-width: 300px">
         <div class="page-title">Donation history</div>
         <v-card class="pa-8" flat elevation="3">
@@ -65,7 +78,7 @@
               :disable-sort="true"
               :headers="headers1"
               :items="desserts1"
-              class="elevation-1"
+              
               :hide-default-footer="true"
             ></v-data-table>
           </div>
@@ -79,7 +92,6 @@
               border=""
               :headers="headers"
               :items="desserts"
-              class="elevation-1"
               :disable-sort="true"
               :hide-default-footer="true"
               :sortable="false"
