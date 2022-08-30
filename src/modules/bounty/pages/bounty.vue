087777a0be1ad43bac560270cb66913e22747336
Bang Bu <daoqtoan@gmail.com>
2022-08-30 21:14:49 +0700

    feat: update banner for bounty list page

--- src/modules/bounty/pages/bounty.vue
+++ src/modules/bounty/pages/bounty.vue
@@ -1,7 +1,8 @@
 <template>
   <v-row dense no-gutters>
     <v-col cols="12">
-      <bounty-banner></bounty-banner>
+      <!-- <bounty-banner></bounty-banner> -->
+      <v-img :src="require('@/assets/images/donationPageBanner.jpg')"></v-img>
     </v-col>
     <!-- <v-col cols="12" v-if="authStore.jwt && vm.currentTaskList.length > 0">
       <v-container>
