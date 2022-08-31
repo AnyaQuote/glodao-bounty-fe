7c57b4b4617d9f30fdf095cfc74eccfd3e0347ca
Bang Bu <daoqtoan@gmail.com>
2022-08-31 16:16:06 +0700

    quick fix: always show metamask

--- src/components/connect-wallet.vue
+++ src/components/connect-wallet.vue
@@ -87,12 +87,10 @@
           </div>
         </v-card>
 
-        <div class="text-h6" v-if="!walletStore.requestingChain || walletStore.requestingChain !== 'sol'">
-          ETH/BSC Chain
-        </div>
+        <div class="text-h6">ETH/BSC Chain</div>
         <v-card
           @click="walletStore.connectViaWalletConnect()"
-          v-if="isMobile && (!walletStore.requestingChain || walletStore.requestingChain !== 'sol')"
+          v-if="isMobile"
           elevation="0"
           outlined
           class="wallet-card neutra100--bg"
@@ -109,13 +107,7 @@
             <v-icon class="mr-2">mdi-chevron-right</v-icon>
           </div>
         </v-card>
-        <v-card
-          @click="walletStore.connectSolidity()"
-          v-else-if="!isMobile && (!walletStore.requestingChain || walletStore.requestingChain !== 'sol')"
-          elevation="0"
-          outlined
-          class="wallet-card neutra100--bg"
-        >
+        <v-card @click="walletStore.connectSolidity()" v-else elevation="0" outlined class="wallet-card neutra100--bg">
           <div class="d-flex align-center neutral100--bg">
             <div class="d-flex align-center ma-4">
               <img width="24" :src="require('@/assets/icons/metamask-fox.svg')" />
