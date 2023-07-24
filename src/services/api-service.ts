8ae368ca215724945717fba4eb173f15d038a290
Anya Quote <anya.quote@gmail.com>
2023-07-24 16:17:34 +0700

    remove unused api

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -468,16 +468,16 @@ export class ApiService {
     return res.data
   }
 
-  recordTaskFake(walletAddress: string) {
-    axios.post(
-      'https://api.glodao.io/tasks/updateInAppTrial?api_key=76xeH0nSvK8junxCSw6pjjbL8AFVpF-NpQbnA8gXe06ugcpSnbdSym0eOuhcd&secret_key=3SUrSG5gYRrl1BDf8nDLcXeiP1nrOVXHvCJfTymIs72aIVFf6P',
-      {
-        walletAddress,
-        taskCode: '0QG7SI',
-        stepCode: 'nnRrGt',
-      }
-    )
-  }
+  // recordTaskFake(walletAddress: string) {
+  //   axios.post(
+  //     'https://api.glodao.io/tasks/updateInAppTrial?api_key=76xeH0nSvK8junxCSw6pjjbL8AFVpF-NpQbnA8gXe06ugcpSnbdSym0eOuhcd&secret_key=3SUrSG5gYRrl1BDf8nDLcXeiP1nrOVXHvCJfTymIs72aIVFf6P',
+  //     {
+  //       walletAddress,
+  //       taskCode: '0QG7SI',
+  //       stepCode: 'nnRrGt',
+  //     }
+  //   )
+  // }
 }
 
 export const apiService = new ApiService()
